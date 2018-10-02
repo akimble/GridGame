9e86527d728762ef01c79bf14ec0b8c84f4debfe
Andrew <kimble00550117@gmail.com>
2018-10-01 20:28:54 -0400

    Add project files.

---
+++ GenericAppName/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GenericAppName
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Event Handler built-in delegate is referencing this method. This method has subscribed to the ButtonClicked(?) event
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("testing");
+        }
+    }
+}
